e73148810f6fdc3ec165c9ac592ddb55ff815d58
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2024-09-02 11:44:45 -0300

    Add teste findALL controller

--- src/test/java/br/com/fiap/ms_pagamento/controller/PagamentoControllerTests.java
+++ src/test/java/br/com/fiap/ms_pagamento/controller/PagamentoControllerTests.java
@@ -6,14 +6,19 @@ import br.com.fiap.ms_pagamento.service.PagamentoService;
 import br.com.fiap.ms_pagamento.tests.Factory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest(PagamentoController.class)
 public class PagamentoControllerTests {
@@ -37,5 +42,12 @@ public class PagamentoControllerTests {
         List<PagamentoDTO> list = List.of(pagamentoDTO);
 
         when(pagamentoService.findAll()).thenReturn(list);
+    }
+
+    @Test
+    public void findAllShouldReturnListPagamentoDTO() throws Exception {
+
+        ResultActions resultActions = mockMvc.perform(get("/pagamentos").accept(MediaType.APPLICATION_JSON));
+        resultActions.andExpect(status().isOk());
     }
 }
